b7b9bf4322836c166d03bf190612c206c51bca89
lyra-planet <1170158548@qq.com>
2022-10-10 22:14:57 +0800

    add unlimited scrolling list && article page && script init-db
    Signed-off-by: lyra-planet <1170158548@qq.com>

--- packages/website/app/types/articleBox.ts
+++ packages/website/app/types/articleBox.ts
@@ -1,22 +1,22 @@
 
 export type IArticleBox = {
-    id:number,
-    type:string,
-    name:string|null,
-    title:string,
-    content:string,
-    createTime:Date,
+	id:number,
+	type:string,
+	name:string|null,
+	title:string,
+	content:string,
+	createTime:Date,
 }|null
 export type IArticle = {
-  id:number,
-  type:string,
-  name:string|null,
-  title:string,
-  content:string,
-  data:string,
-  createTime:Date,
+	id:number,
+	type:string,
+	name:string|null,
+	title:string,
+	content:string,
+	data:string,
+	createTime:Date,
 }|null
 export type ILoaderData = {
-        articleBoxListData:
-        Array<IArticleBox>
-      }
+	articleBoxListData:
+	Array<IArticleBox>
+}

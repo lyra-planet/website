4c19263c58d59f5cec07f908eb7516959405f9ba
lyra-planet <1170158548@qq.com>
2022-10-12 22:57:26 +0800

    add pagination
    Signed-off-by: lyra-planet <1170158548@qq.com>

---
+++ packages/website/app/routes/api/home/$pagination.tsx
@@ -0,0 +1,33 @@
+import type { LoaderFunction } from '@remix-run/server-runtime'
+import type { IArticleBox } from '~/types/articleBox'
+
+import { db } from '~/utils/db.server'
+
+
+export const loader: LoaderFunction = async ({ params }) => {
+	const index = parseInt(params.pagination as string)||0
+	const data = await getData(index)
+	return data
+}
+
+const getData = async (myCursor:number) => {
+	const data: IArticleBox[] = await db.articleBox.findMany({
+		take:6,
+		cursor: {
+			id: myCursor,
+		},
+		select: {
+			id: true,
+			type:true,
+			name: true,
+			title: true,
+			content: true,
+			createTime: true,
+		},
+		orderBy:{
+			id:'desc'
+		}
+	})
+	return data
+
+}
